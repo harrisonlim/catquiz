33906f89e4958b6de568a57d8ead073c8297c22c
harrisonlim <harrison.lim.hx@gmail.com>
2016-02-01 08:12:39 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catquiz::Application.config.secret_key_base = '8f1683f093706d353034b53c8f76e230adb1d83394a5d25e8c491de792ec0f17151e886621ba695c2d999214d94a7906b8920de455808fd55718fac8451f4364'
